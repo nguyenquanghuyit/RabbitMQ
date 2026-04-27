907bfcd8492d6125a4a3bc98c9d2c90e1c74e851
nguyenquanghuyit <mr.huy.it.cklk@gmail.com>
2026-04-27 15:22:29 +0700

    First commit

---
+++ RabbitMQ/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿namespace RabbitMQ
+{
+    partial class frmRabbitMQ
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtUserName = new System.Windows.Forms.TextBox();
+            this.txtConversation = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnSend = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(22, 52);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 21);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "From";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtUserName
+            // 
+            this.txtUserName.Location = new System.Drawing.Point(155, 52);
+            this.txtUserName.Name = "txtUserName";
+            this.txtUserName.Size = new System.Drawing.Size(418, 28);
+            this.txtUserName.TabIndex = 1;
+            // 
+            // txtConversation
+            // 
+            this.txtConversation.Location = new System.Drawing.Point(155, 86);
+            this.txtConversation.Multiline = true;
+            this.txtConversation.Name = "txtConversation";
+            this.txtConversation.Size = new System.Drawing.Size(418, 183);
+            this.txtConversation.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 89);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(91, 21);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Conversion";
+            this.label2.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(155, 275);
+            this.txtMessage.Multiline = true;
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(418, 78);
+            this.txtMessage.TabIndex = 1;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 275);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 21);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Write Messager";
+            this.label3.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(155, 374);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(99, 42);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Send";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
+            // 
+            // frmRabbitMQ
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 21F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 428);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
+            this.Controls.Add(this.txtConversation);
+            this.Controls.Add(this.txtUserName);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Tahoma", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmRabbitMQ";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Chat with RabbitMQ Client";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtUserName;
+        private System.Windows.Forms.TextBox txtConversation;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnSend;
+    }
+}
+
---
+++ RabbitMQ/Form1.cs
@@ -0,0 +1,86 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace RabbitMQ
+{
+    public partial class frmRabbitMQ : Form
+    {
+        string exchangeName = "RabbitHOB";
+        string clientId = Guid.NewGuid().ToString();
+        IConnection connection = null;
+        IModel channelSend = null;
+        IModel channelReceive = null;
+        public frmRabbitMQ()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+            string input = txtUserName.Text + " > " + txtMessage.Text;
+            byte[] message = Encoding.UTF8.GetBytes(input);
+            channelSend.BasicPublish(exchangeName, "", null, message);
+            txtMessage.Text = string.Empty;
+            txtMessage.Focus();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (channelSend.IsOpen) channelSend.Close();
+            if (channelReceive.IsOpen) channelReceive.Close();
+            if (connection.IsOpen) connection.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            txtUserName.Text = clientId.Substring(0, 13);
+            txtConversation.Text = "Welcome!\r\n";
+            var connectionFactory = new ConnectionFactory
+            {
+                HostName = "localhost",
+                Port = 5672,
+                UserName = "guest",
+                Password = "guest",
+                VirtualHost = "/"
+            };
+
+            connection = connectionFactory.CreateConnection();
+            channelSend = connection.CreateModel();
+            channelSend.ExchangeDeclare(exchangeName, ExchangeType.Fanout, false, true, null);
+            channelReceive = connection.CreateModel();
+            channelReceive.QueueDeclare(clientId, false, false, true, null);
+            channelReceive.QueueBind(clientId, exchangeName, "");
+            var consumer = new EventingBasicConsumer(channelReceive);
+            channelReceive.BasicConsume(clientId, true, consumer);
+            consumer.Received += (s, ee) =>
+            {
+                string message = Encoding.UTF8.GetString(ee.Body) + "\r\n";
+                txtConversation.BeginInvoke(new Action(() =>
+                {
+                    txtConversation.Text += message;
+                    ScrollToEnd(txtConversation);
+                }));
+            };
+            txtMessage.Focus();
+        }
+
+        public void ScrollToEnd(TextBox textbox)
+        {
+            textbox.Select(textbox.Text.Length - 1, 0);
+            textbox.ScrollToCaret();
+        }
+    }
+}
